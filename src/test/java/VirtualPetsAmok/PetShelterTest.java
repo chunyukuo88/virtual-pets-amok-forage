02c2ac8483d6eefc6799c18f7424f20adc8242cb
Cholliex <ochollieoh@yahoo.com>
2019-09-10 15:14:04 -0400

    petShelterTest

--- src/test/java/VirtualPetsAmok/PetShelterTest.java
+++ src/test/java/VirtualPetsAmok/PetShelterTest.java
@@ -1,11 +1,20 @@
 package VirtualPetsAmok;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class PetShelterTest {
 
 	@Test
+	
+	
+	
 	public void addPetToShelter() {
+		PetShelter underTest = new PetShelter();
+		VirtualPet petUnderTest = new VirtualPet("chupacabra", 5,5,5);
+		underTest.petsInTheShelter.add(petUnderTest);
+		assertEquals(underTest.petsInTheShelter.get(0), (petUnderTest));
 		
 	}
 	

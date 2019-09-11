c96c064c74898d0d37e58ee8eeb2667e5fb6ef6a
unknown <alexgochenour@gmail.com>
2019-09-11 14:19:27 -0400

    Responding to Bash's request to commit.

--- src/main/java/VirtualPetsAmok/PetShelter.java
+++ src/main/java/VirtualPetsAmok/PetShelter.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 
 public class PetShelter {
 
-	ArrayList<VirtualPet> petsInTheShelter = new ArrayList<VirtualPet>();
+	ArrayList<VirtualPet> petsInTheShelter = new ArrayList<>();
 
+		public void petAdder(VirtualPet) {
+			
+		}
+
+		
+		VirtualPet tortoise = new VirtualPet("Chuck Tortoise", 5, 5, 5);
+		petsInTheShelter.add(tortoise);
+		
+		VirtualPet lorax = new VirtualPet("Chuck Lorax", 4, 4, 4);
+		petsInTheShelter.add(lorax);
+		
+		for (VirtualPet pet : petsInTheShelter)
+			System.out.println(pet.petName);
 }
